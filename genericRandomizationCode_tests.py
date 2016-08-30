2f558fd56e5581a282444d8898f20129e3502575
asonntag <asonntag@aflcio.org>
2016-08-30 15:30:54 -0400

    Created function for printing main variable and functions checks within randomization object. Added syntax to check different dataframe besides the sample data 'binary.csv'

--- genericRandomizationCode_tests.py
+++ genericRandomizationCode_tests.py
@@ -3,22 +3,23 @@ import pandas as pd
 import statsmodels.api as sm
 from genericRandomizationCode import randomization
 
-df = pd.read_csv("http://www.ats.ucla.edu/stata/data/binary.csv") # for some reason only works in ipython notebook but returns parsing error when run from command line
+def testPrint(rdm):
+    print('seed: ' + str(rdm.seed))
+    print('number of conditions: ' + str(rdm.numConditions))
+    print('minimum p value: ' + str(rdm.minPval))
+    print('Data Frame: ' + str(df))
+    print('Strata Column Name: ' + str(rdm.strataName))
+    print(rdm.randomStrata())
 
 #test randomization with user definitions
+'''
+df = pd.read_csv("binary.csv")
 rdm = randomization(df, seed=12823, minPval=.1, numConditions=3, balanceVars=['gpa','gre'])
-print('seed: ' + str(rdm.seed))
-print('number of conditions: ' + str(rdm.numConditions))
-print('minimum p value: ' + str(rdm.minPval))
-print('Data Frame: ' + str(df))
-print('Strata Column Name: ' + rdm.strataName)
-print(rdm.randomStrata())
+testPrint(rdm)
+rdm.reRandomize()
+'''
 
-#test without user defs
-rdm2 = randomization()
-print('seed: ' + str(rdm2.seed))
-print('number of conditions: ' + str(rdm2.numConditions))
-print('minimum p value: ' + str(rdm2.minPval))
-print('Data Frame: ' + str(df))
-print('Strata Column Name: ' + rdm2.strataName)
-print(rdm2.randomStrata())
+df = pd.DataFrame({'humans':['Audrey', 'Grace', 'Joe', 'Paul', 'Genny'], 'myStrata':['F', 'F', 'M', 'M', 'F']})
+rdm = randomization(df, strataName='myStrata', seed=12823, minPval=.1, numConditions=3, balanceVars=['humans'])
+testPrint(rdm)
+rdm.reRandomize()
